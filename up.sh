11d274713cac4943fc512a76aaa30d85a4460c68
Fernando <fernandoemiliolopez13@gmail.com>
2026-01-15 19:42:42 -0600

    ✨ Update environment configuration to use CAP_PORT variable and enhance deployment script for better user interaction

--- up.sh
+++ up.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-docker compose -f docker/dev/compose.yaml up -d   
-echo "Shipped"
+printf "¿You like to deploy or down the container? (deploy/down): "
+read -r ans
+
+if [[ "$ans" == "deploy" ]]; then
+	docker compose -f docker/dev/compose.yaml up -d
+	printf "Shipped\n"
+else
+	docker compose -f docker/dev/compose.yaml down
+	printf "Removed\n"
+fi
